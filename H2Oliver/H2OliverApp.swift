aff1f2f5ac690e787c7b3d60609dd1c9e3b7439a
Patrick Lostaunau <jplosta01@gmail.com>
2026-06-21 14:58:22 -0500

    Initial Commit

---
+++ H2Oliver/H2OliverApp.swift
@@ -0,0 +1,17 @@
+//
+//  H2OliverApp.swift
+//  H2Oliver
+//
+//  Created by Patrick Lostaunau on 21/06/26.
+//
+
+import SwiftUI
+
+@main
+struct H2OliverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
